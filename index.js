05cf663a61438265553a56280509854a460e1461
Jotirmoy <jotirmoyff1976@gmail.com>
2025-01-31 23:00:17 +0600

    thired comt

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import userschema from './models/schema.js';
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 
-app.use(express.json());
+app.use(express.json());    
 app.use(express.urlencoded({ extended: true }));
 
 
